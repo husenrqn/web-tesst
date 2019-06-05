c95e249dbccbad129050b0a1038ab18d66c8acc8
husenrqn <husen11ipa@gmail.com>
2019-06-06 06:08:18 +0700

    First commit

---
+++ a.js
@@ -0,0 +1,35 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(230, 104, 149,0.5)";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "#E66895";
+}
+//script2
+function openNav2() {
+    document.getElementById("mySidenav2").style.width = "250px";
+    document.getElementById("main2").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(230, 104, 149,0.5)";
+}
+function closeNav2() {
+    document.getElementById("mySidenav2").style.width = "0";
+    document.getElementById("main2").style.marginLeft= "0";
+    document.body.style.backgroundColor = "#E66895";
+}
+//script3
+function openNav3() {
+    document.getElementById("mySidenav3").style.width = "250px";
+    document.getElementById("main3").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(230, 104, 149,0.5)";
+}
+function closeNav3() {
+    document.getElementById("mySidenav3").style.width = "0";
+    document.getElementById("main3").style.marginLeft= "0";
+    document.body.style.backgroundColor = "#E66895";
+}
+    document.getElementById("mySidenav2").style.width = "0";
+    document.getElementById("main2").style.marginLeft= "0";
+    document.body.style.backgroundColor = "#E66895";
